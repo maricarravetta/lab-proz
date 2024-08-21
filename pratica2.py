c4e7ccc9a5ada00805df3b715f886036e588e539
Mariana Carravetta <carravettamm@gmail.com>
2024-08-21 16:07:13 -0300

    Add files via upload

---
+++ pratica2.py
@@ -0,0 +1,18 @@
+def calculadora(n1, n2, calculo):
+    if(calculo == 1):
+        return n1 + n2
+    elif (calculo == 2):
+        return n1 - n2
+    elif (calculo == 3):
+        return n1 / n2
+    elif (calculo == 4):
+        return n1 * n2
+    else:
+        return 0
+
+n1 = input("Digite o primeiro número")
+n2 = input("Digite o segundo número")
+calculo = input("Qual é a operação que você deseja fazer?\n Para soma, digite 1\n Para subtração, digite 2\n Para divisão, digite 3\n Para multiplicação, digite 4")
+
+resultado = calculadora(n1, n2, calculo)
+print(resultado)
