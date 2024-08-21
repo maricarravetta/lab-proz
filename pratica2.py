21ef0726218da349e2e8b52c4f31b420e6f62e3f
Mariana Carravetta <carravettamm@gmail.com>
2024-08-21 17:34:58 -0300

    Update pratica2.py

--- pratica2.py
+++ pratica2.py
@@ -10,9 +10,9 @@ def calculadora(n1, n2, calculo):
     else:
         return 0
 
-n1 = input("Digite o primeiro número")
-n2 = input("Digite o segundo número")
-calculo = input("Qual é a operação que você deseja fazer?\n Para soma, digite 1\n Para subtração, digite 2\n Para divisão, digite 3\n Para multiplicação, digite 4")
+n1 = int(input("Digite o primeiro número"))
+n2 = int(input("Digite o segundo número"))
+calculo = int(input("Qual é a operação que você deseja fazer?\n Para soma, digite 1\n Para subtração, digite 2\n Para divisão, digite 3\n Para multiplicação, digite 4"))
 
 resultado = calculadora(n1, n2, calculo)
 print(resultado)
